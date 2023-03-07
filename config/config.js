b81899e70ffb2d5776871e3abe03cf5258210958
0xpwnstar <chatna717@gmail.com>
2023-03-07 17:11:49 +0530

    patched mysql error 17

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path:__dirname+'/../../.env'});
+require('dotenv').config({path:__dirname+'/../../../.env'});
 console.log(process.env.NAME)
 module.exports= {
   "development": {
