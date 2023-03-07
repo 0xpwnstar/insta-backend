be3e76b09550c2928c9a33ba2047f7de63633830
0xpwnstar <chatna717@gmail.com>
2023-03-07 16:39:56 +0530

    patched mysql error 8

---
+++ config/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports= {
+  "development": {
+    "username": process.env.NAME,
+    "password": process.env.PASSWORD,
+    "database": "insta_backend",
+    "host": process.env.HOST,
+    "dialect": "mysql"
+  }
+}
--- index.js
+++ index.js
@@ -2,16 +2,11 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const PORT = process.env.PORT || 4111;
-const sequelize = require('./sequelize')
+require('dotenv').config();
 
 app.use('/', async (req,res) => { 
     
-    sequelize.authenticate().then(async () => {
-        console.log('Connection has been established successfully.');
-        res.json({"Success":1})
-    }).catch((error) => {
-        console.error('Unable to connect to the database: ', error);
-    });
+    
 
 }) 
 app.use('/api', require('./routes/index'));
