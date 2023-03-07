8b4da826a56bbb3881b8165fd30bf785ae0a4967
0xpwnstar <chatna717@gmail.com>
2023-03-07 12:09:17 +0530

    added sequelize, mysql connection

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
-
+const sequelize = require('./models/sequelize');
 const PORT = process.env.PORT || 4111;
-app.listen(PORT, console.log("Listenening on",  PORT) );
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established')
+}).catch((error) => {
+    console.log(error)
+})
+app.use('/api', require('./routes/index'));
+app.listen(PORT, console.log("Listenening on ",  PORT) );
