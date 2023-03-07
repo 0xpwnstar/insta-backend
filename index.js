0f1273d486ad97f039b48158c0cae23a3800fa36
0xpwnstar <chatna717@gmail.com>
2023-03-07 14:37:52 +0530

    patched mysql error 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const PORT = process.env.PORT || 4111;
-const  sequelize = require('./models') 
+const  sequelize = require('./models/index') 
 app.use('/', async (req,res) => { 
     
     sequelize.authenticate().then(async () => {
