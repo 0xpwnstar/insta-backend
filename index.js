51d0a249c890139da65b8300e39b744866ffdc96
0xpwnstar <chatna717@gmail.com>
2023-03-07 14:11:00 +0530

    patched mysql error

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(
-    'insta_backend',
-    'admin',
-    'C7508tan!',
-     {
-       host: 'database-2.cluster-ro-cyrq7glzuytr.ap-south-1.rds.amazonaws.com',
-       dialect: 'mysql'
-     }
-);
 const PORT = process.env.PORT || 4111;
-app.use('/', async => { 
+app.use('/', async (req,res) => { 
+    const sequelize = new Sequelize(
+        'insta_backend',
+        'admin',
+        'C7508tan!',
+         {
+           host: 'database-2.cluster-ro-cyrq7glzuytr.ap-south-1.rds.amazonaws.com',
+           dialect: 'mysql'
+         }
+    );
     sequelize.authenticate().then(() => {
         console.log('Connection has been established successfully.');
+        res.json({"Success":1})
+        res.send()
     }).catch((error) => {
         console.error('Unable to connect to the database: ', error);
     });
