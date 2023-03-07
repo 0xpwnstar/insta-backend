16179855e5f66d4e58301cf4f0fc7834ddfaf27d
0xpwnstar <chatna717@gmail.com>
2023-03-07 16:55:14 +0530

    patched mysql error 9

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
+const models = require('./models')
 require('dotenv').config()
 const PORT = process.env.PORT || 4111;
 require('dotenv').config();
 
 app.use('/', async (req,res) => { 
-    
-    
-
+    const result = await sequelize.query("show databases;")
+    res.json({result})
 }) 
 app.use('/api', require('./routes/index'));
 app.listen(PORT, console.log("Listenening on ",  PORT) );
