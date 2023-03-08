9079dda2f0a571064b78401f199d30f736dbfc9d
0xpwnstar <chatna717@gmail.com>
2023-03-08 15:13:11 +0530

    added signup controller

---
+++ controllers/user.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto')
+const models = require('../models')
+
+
+
+exports.signup =async (req,res) => {
+    res.send(models)
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const  express = require('express');
 const router = express.Router();
-
+const models = require('../models');
+const User = require('../controllers/user')
 router.get('/', (req, res) => res.send("You are visiting Mars "))
+// router.post('/login',(req, res) => User.login)
+router.post('/signup',(req,res) => User.signup)
 
 module.exports = router;
