696d3e0fec8fdec1def16d05feedc01709f050f4
0xpwnstar <chatna717@gmail.com>
2023-03-08 16:05:05 +0530

    added signup controller 15

--- controllers/user.js
+++ controllers/user.js
@@ -5,18 +5,18 @@ const models= require('../models')
 
 exports.signup = async (req,res) => {
     const body = req.body
-    console.log(await models.user)
     if (!body || !body.email ){
         res.send({
             Success: false
         })
     }
-    const exists = await models.user.findAll({
-        where: {
-            email: {
-                [models.sequelize.Op.eq]: body.email
-            }
-        }
-})
-    res.send(exists)
+    console.log(models.Sequelize)
+//     const exists = await models.user.findAll({
+//         where: {
+//             email: {
+//                 [models.sequelize.Op.eq]: body.email
+//             }
+//         }
+// })
+    res.send("exists")
 }
