ba0f09a46a376d88d02837e6f2aa7f826912cff1
0xpwnstar <chatna717@gmail.com>
2023-03-08 15:58:29 +0530

    added signup controller 13

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ const models= require('../models')
 exports.signup = async (req,res) => {
     const body = req.body
     console.log(await models.user)
-    if (!body.email || !body){
+    if (!body || !body.email ){
         res.send({
             Success: false
         })
