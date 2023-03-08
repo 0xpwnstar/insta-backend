c20c560ce28bd29e6dfd4bebe419377c7cf81b71
0xpwnstar <chatna717@gmail.com>
2023-03-08 15:18:40 +0530

    added signup controller 3

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const models = require('../models');
 const { signup } = require('../controllers/user')
 router.get('/', (req, res) => res.send("You are visiting Mars "))
 // router.post('/login',(req, res) => User.login)
-router.post('/signup',(req,res) => signup)
+router.get('/signup',(req,res) => signup)
 
 module.exports = router;
