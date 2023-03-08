d691ea52a15a2b9a8f3a6be42062d36a169315f6
0xpwnstar <chatna717@gmail.com>
2023-03-08 21:49:45 +0530

    patched login controller 2

--- controllers/user.js
+++ controllers/user.js
@@ -58,10 +58,9 @@ exports.login = async (req,res) => {
         })
         return 0
     }
-    let salt;
     let password;
     const exists = await models.user.findAll({
-        attributes: ['password',  'salt'],
+        attributes: ['id','password',  'salt'],
         where: {
             email: {
                 [models.Sequelize.Op.eq]: body.email
@@ -69,29 +68,22 @@ exports.login = async (req,res) => {
         }
     })
     let user = null;
-    // if (!exists.length){
-    //     salt = await crypto.randomBytes(12).toString('hex').slice(0,12);
-    //     if (body.password) {
-    //         password = await crypto.createHmac('sha256',salt).update(body.password).digest('hex');
-    //         if(body.email && body.firstname && body.lastname){
-    //             user = await models.user.create({
-    //                 firstname: body.firstname,
-    //                 lastname: body.lastname,
-    //                 email: body.email,
-    //                 password: password,
-    //                 salt: salt
-    //             })
-    //         }
-    //     }else{
-    //         res.send({
-    //             Success: false,
-    //             Password: null
-    //         })
-    //         return 0
-    //     }
-    // }
+    if (exists.length){
+        if (body.password) {
+            password = await crypto.createHmac('sha256',exists[0].salt).update(body.password).digest('hex');
+            if (password == exists[0].password){
+                user = exists[0].id
+            }
+        }else{
+            res.send({
+                Success: false,
+                Password: null
+            })
+            return 0
+        }
+    }
 
-    res.json({exists})
+    res.json({user})
     res.send()
     
 }
