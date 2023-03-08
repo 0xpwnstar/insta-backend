8fc945f398e001ab90628b8a0cbeac05c0d0d105
0xpwnstar <chatna717@gmail.com>
2023-03-08 20:56:13 +0530

    added signup controller 17

--- controllers/user.js
+++ controllers/user.js
@@ -9,6 +9,7 @@ exports.signup = async (req,res) => {
         res.send({
             Success: false
         })
+        return 0
     }
 
     const exists = await models.user.findAll({
@@ -17,6 +18,32 @@ exports.signup = async (req,res) => {
                 [models.Sequelize.Op.eq]: body.email
             }
         }
-})
-    res.send("exists")
+    })
+    const salt;
+    const password;
+    if (!exists){
+        salt = crypto.randomBytes(12).toString('hex').slice(0,12);
+        if (body.password) {
+            password = crypto.createHmac('sha256',salt).update(body.password).digest('hex');
+        }else{
+            res.send({
+                Success: false,
+                Password: null
+            })
+            return 0
+        }
+    }
+    const user = null
+    if(body.email && body.firstname && body.lastname){
+        user = await models.user.create({
+            firstname: body.firstname,
+            lastname: body.lastname,
+            email: body.email,
+            password,
+            salt
+        })
+    }
+    res.json({user})
+    res.send()
+    
 }
