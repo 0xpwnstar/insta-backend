57c3fbe6c4e7c277fed32766f093343a59e3e496
0xpwnstar <chatna717@gmail.com>
2023-03-10 20:20:45 +0530

    patched follow_activity model 26

--- controllers/follow.js
+++ controllers/follow.js
@@ -3,5 +3,15 @@ const { authorize } = require('./user')
 
 exports.followUser = async (req, res) => {
     let user = req.body.user
-    
+    let follow = req.body.follow
+    if (follow && user && follow != user){
+        const followActivity = await models.follow.create({
+            userId: user,
+            followingId: follow
+        })
+        return res.json({ followActivity }).send()
+    }
+    res.send({
+        Success: false
+    })
 }
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@ const  express = require('express');
 const router = express.Router();
 const models = require('../models');
 const { signup, login, authorize } = require('../controllers/user')
+const { followUser } = require('../controllers/follow')
+
 router.get('/', (req, res) => res.send("You are visiting Mars "))
 // router.post('/login',(req, res) => User.login)
 router.post('/signup',signup)
 router.post('/login',login)
 let uid = null
 router.post('*',async (req, res, next) =>{
+    let body = req.body
     uid = await authorize(req,res)
-    if (uid){
+    if (uid.user == body.user){
         res.json({uid})
         next('route')
     } else res.json({Success: false})
@@ -17,8 +20,6 @@ router.post('*',async (req, res, next) =>{
     res.json({Success: false})
 })
 
-router.post('/follow', async (req, res, next) => {
-    res.send()
-})
+router.post('/follow', followUser)
 
 module.exports = router;
