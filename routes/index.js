612aaf9378b7c11c0da55f353cd6761cd5238486
0xpwnstar <chatna717@gmail.com>
2023-03-23 18:05:34 +0530

    Patched user 4

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.post('*',async (req, res, next) =>{
     uid = await authorize(req,res)
     if (uid.user == body.user){
         next('route')
-    } else res.json({Success: "Cookie error"})
+    } else res.json({Success: "Cookie error 1"})
 },(req, res, next) =>{
     res.json({Success: "Cookie error"})
 })
